328bb4d7aec37128685260532f6eca2a0a0ffcfb
Alann Maulana <alann_it06@yahoo.co.id>
2014-09-16 08:47:30 +0700

    Display brochure image

---
+++ src/com/eyro/cubeacon/demos/BeaconActivity.java
@@ -0,0 +1,34 @@
+package com.eyro.cubeacon.demos;
+
+import android.app.Activity;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+public class BeaconActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beacon);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        
+        Bundle bundle = getIntent().getExtras();
+        String path = bundle.getString(MainActivity.EXTRA_INTENT);
+        
+        ImageView image = (ImageView) findViewById(R.id.beacon_brochure);
+        image.setImageDrawable(Drawable.createFromPath(path));;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(menuItem);
+        }
+    }
+}
--- src/com/eyro/cubeacon/demos/MainActivity.java
+++ src/com/eyro/cubeacon/demos/MainActivity.java
@@ -2,10 +2,14 @@ package com.eyro.cubeacon.demos;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ScrollView;
@@ -15,45 +19,72 @@ import com.eyro.cubeacon.CBApp;
 import com.eyro.cubeacon.CBBeacon;
 
 public class MainActivity extends Activity {
-    String tag = "MainActivity";
-    ProgressDialog progress;
-    TextView       textDemos;
-    ScrollView main;
-    
+    String                     tag             = "MainActivity";
+    ProgressDialog             progress;
+    TextView                   textDemos;
+    ScrollView                 main;
+    static NotificationManager notificationManager;
+    static final int           NOTIFICATION_ID = 123456;
+    public static final String EXTRA_INTENT    = "NOTIFICATION";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         CBApp.createService(this);
+        notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         setContentView(R.layout.activity_main);
         textDemos = (TextView) findViewById(R.id.textDemos);
         main = (ScrollView) findViewById(R.id.main_view);
         CBApp.setBeaconListener(this, new CBApp.BeaconListener() {
-            
+
             @Override
             public void onExit(CBBeacon beacon, long timeInterval) {
                 // TODO Auto-generated method stub
-                main.setBackgroundColor(Color.WHITE);
+                //main.setBackgroundColor(Color.WHITE);
                 Log.i(tag, beacon.getName() + " exited, major: " + beacon.getMajor() + ", minor: " + beacon.getMinor());
             }
-            
+
             @Override
             public void onEnter(CBBeacon beacon) {
                 // TODO Auto-generated method stub
-                main.setBackgroundColor(beacon.getColor());
-                textDemos.setText(beacon.getName());
+                //main.setBackgroundColor(beacon.getColor());
+                //textDemos.setText(beacon.getName());
+                showNotif(beacon.getName() + " entered region", beacon.getBrochureLocalPath());
                 Log.i(tag, beacon.getName() + " entered, major: " + beacon.getMajor() + ", minor: " + beacon.getMinor());
             }
-            
+
             @Override
             public void onChange(CBBeacon old, CBBeacon current) {
                 // TODO Auto-generated method stub
-                main.setBackgroundColor(current.getColor());
-                textDemos.setText(current.getName());
+                //main.setBackgroundColor(current.getColor());
+                //textDemos.setText(current.getName());
+                main.setBackground(Drawable.createFromPath(current.getBrochureLocalPath()));
                 Log.i(tag, current.getName() + " changed, major: " + current.getMajor() + ", minor: " + current.getMinor());
             }
         });
     }
 
+    void showNotif(String msg, String brochurePathFile){
+        Intent notifyIntent = new Intent(MainActivity.this, BeaconActivity.class);
+        notifyIntent.putExtra(MainActivity.EXTRA_INTENT, brochurePathFile);
+        notifyIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivities(
+                MainActivity.this,
+                0,
+                new Intent[]{notifyIntent},
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        Notification notification = new Notification.Builder(MainActivity.this)
+        .setSmallIcon(R.drawable.ic_launcher)
+        .setContentTitle("Notify Demo")
+        .setContentText(msg)
+        .setAutoCancel(true)
+        .setContentIntent(pendingIntent)
+        .build();
+        notification.defaults |= Notification.DEFAULT_SOUND;
+        notification.defaults |= Notification.DEFAULT_LIGHTS;
+        notificationManager.notify(NOTIFICATION_ID, notification);
+    }
+    
     @Override
     public void onBackPressed() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -73,21 +104,21 @@ public class MainActivity extends Activity {
         });
         AlertDialog alert = builder.create();
         alert.show();
-        
-    }
 
+    }
+    
     @Override
     protected void onStart() {
         CBApp.startService(this);
         super.onStart();
     }
-
+    
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         CBApp.onActivityResult(requestCode, resultCode, data);
         super.onActivityResult(requestCode, resultCode, data);
     }
-
+    
     @Override
     protected void onDestroy() {
         CBApp.destroyService(this);
