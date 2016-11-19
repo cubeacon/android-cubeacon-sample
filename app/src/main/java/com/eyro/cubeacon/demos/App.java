d59d8ebc75219878ec8440f37e5cd5b4dd5a4f3c
Cubeacon <eyro.cubeacon@gmail.com>
2016-11-19 11:34:15 +0700

    Update v2.0.0

--- app/src/main/java/com/eyro/cubeacon/demos/App.java
+++ app/src/main/java/com/eyro/cubeacon/demos/App.java
@@ -1,21 +1,82 @@
 package com.eyro.cubeacon.demos;
 
 import android.app.Application;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.util.Log;
 
-import com.eyro.cubeacon.CBApp;
+import com.eyro.cubeacon.CBBootstrapListener;
+import com.eyro.cubeacon.CBBootstrapRegion;
+import com.eyro.cubeacon.CBRegion;
+import com.eyro.cubeacon.Cubeacon;
+import com.eyro.cubeacon.LogLevel;
+import com.eyro.cubeacon.Logger;
+import com.eyro.cubeacon.MonitoringState;
+
+import java.util.UUID;
 
 /**
  * Created by Eyro on 10/31/15.
  */
-public class App extends Application {
+public class App extends Application implements CBBootstrapListener {
+    private static final String TAG = App.class.getSimpleName();
+
     @Override
     public void onCreate() {
         super.onCreate();
 
-        // settings for enable debug logging
-        CBApp.enableDebugLogging();
+        // set Cubeacon SDK log level to verbose mode
+        Logger.setLogLevel(LogLevel.VERBOSE);
 
-        // initialize Cubeacon SDK
-        CBApp.initialize(this, "Cubeacon.properties");
+        // enable background power saver to save battery life up to 60%
+        Cubeacon.setBackgroundPowerSaver(true);
+
+        // initializing Cubeacon SDK
+        Cubeacon.initialize(this);
+
+        // setup region scanning when OS boot completed
+        CBBootstrapRegion.setup(this,
+                new CBRegion("com.eyro.cubeacon.bootstrap_region",
+                        UUID.fromString("cb10023f-a318-3394-4199-a8730c7c1aec")));
+
+
+
+    }
+
+    @Override
+    public void didEnterRegion(CBRegion region) {
+        Log.d(TAG, "Entering region: " + region);
+    }
+
+    @Override
+    public void didExitRegion(CBRegion region) {
+        Log.d(TAG, "Exiting region: " + region);
+    }
+
+    @Override
+    public void didDetermineStateForRegion(MonitoringState state, CBRegion region) {
+        Log.d(TAG, "Region: " + region + ", state: " + state.name());
+    }
+
+    public void showNotification(String title, String message) {
+        Intent notifyIntent = new Intent(this, MonitoringActivity.class);
+        notifyIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivities(this, 1234,
+                new Intent[] { notifyIntent }, PendingIntent.FLAG_UPDATE_CURRENT);
+        Notification notification = new Notification.Builder(this)
+                .setSmallIcon(android.R.drawable.ic_dialog_info)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
+                .build();
+        notification.defaults |= Notification.DEFAULT_SOUND;
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(5678, notification);
     }
 }
---
+++ app/src/main/java/com/eyro/cubeacon/demos/RangingActivity.java
@@ -0,0 +1,142 @@
+package com.eyro.cubeacon.demos;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.RemoteException;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import com.eyro.cubeacon.CBBeacon;
+import com.eyro.cubeacon.CBRangingListener;
+import com.eyro.cubeacon.CBRegion;
+import com.eyro.cubeacon.CBServiceListener;
+import com.eyro.cubeacon.Cubeacon;
+import com.eyro.cubeacon.SystemRequirementManager;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+public class RangingActivity extends AppCompatActivity implements CBRangingListener, CBServiceListener, AdapterView.OnItemClickListener {
+
+    private static final String TAG = RangingActivity.class.getSimpleName();
+    private Cubeacon cubeacon;
+    private ListView listView;
+    private SimpleAdapter adapter;
+    private List<Map<String, String>> data;
+    private List<CBBeacon> beacons;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ranging);
+
+        // assign view
+        listView = (ListView) findViewById(R.id.listview);
+
+        // set default adapter
+        String[] from = new String[]{"title", "subtitle"};
+        int[] to = new int[]{android.R.id.text1, android.R.id.text2};
+        data = new ArrayList<>();
+        adapter = new SimpleAdapter(this, data, android.R.layout.simple_list_item_2, from, to);
+
+        // set adapter to listview
+        listView.setAdapter(adapter);
+
+        // set listview on item click listener
+        listView.setOnItemClickListener(this);
+
+        // assign local instance of Cubeacon manager
+        cubeacon = Cubeacon.getInstance();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // check all requirement like is BLE available, is bluetooth on/off,
+        // location service for Android API 23 or later
+        if (SystemRequirementManager.checkAllRequirementUsingDefaultDialog(this)) {
+            // connecting to Cubeacon service when all requirements completed
+            cubeacon.connect(this);
+            // disable background mode, because we're going to use full
+            // scanning resource in foreground mode
+            cubeacon.setBackgroundMode(false);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // enable background mode when this activity paused
+        cubeacon.setBackgroundMode(true);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // disconnect from Cubeacon service when this activity destroyed
+        cubeacon.disconnect(this);
+    }
+
+    @Override
+    public void didRangeBeaconsInRegion(final List<CBBeacon> beacons, CBRegion region) {
+        this.beacons = beacons;
+
+        String title, subtitle;
+        Map<String, String> map;
+
+        // clear data
+        data.clear();
+        for (CBBeacon beacon : beacons) {
+            title = beacon.getProximityUUID().toString().toUpperCase();
+            subtitle = String.format(Locale.getDefault(), "M:%d m:%d RSSI:%d Accuracy:%.2fm",
+                    beacon.getMinor(), beacon.getMinor(), beacon.getRssi(), beacon.getAccuracy());
+            map = new HashMap<>();
+            map.put("title", title);
+            map.put("subtitle", subtitle);
+            data.add(map);
+        }
+
+        // update view using runnable
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                adapter.notifyDataSetChanged();
+                if (getSupportActionBar() != null) {
+                    getSupportActionBar().setSubtitle("Ranged beacon : " + beacons.size());
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onBeaconServiceConnect() {
+        // add ranging listener implementation
+        cubeacon.addRangingListener(this);
+        try {
+            // create a new region for ranging beacons
+            CBRegion region = new CBRegion("com.eyro.cubeacon.ranging_region");
+            // start ranging beacons using region
+            cubeacon.startRangingBeaconsInRegion(region);
+        } catch (RemoteException e) {
+            Log.e(TAG, "Error while start ranging beacon, " + e);
+        }
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        CBBeacon beacon = this.beacons.get(i);
+        Intent intent = new Intent(this, MonitoringActivity.class);
+        intent.putExtra(MonitoringActivity.INTENT_BEACON, beacon);
+        startActivity(intent);
+    }
+}
